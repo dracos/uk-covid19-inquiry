bb092d9fd53ec33e4f45a49ccfba5842cad82d67
Matthew Somerville <matthew@mysociety.org>
2024-10-07 23:30:46 +0100

    Spot th in date.

--- parse.py
+++ parse.py
@@ -170,7 +170,7 @@ def parse_transcript(url, text):
             line = line.replace('**', r'\*\*')
 
             # Date at start
-            m = re.match(r' *(Mon|Tues|Wednes|Thurs|Fri)day,? \d+(nd)? (August|September|October|November|December|January|February|March|April|May|June|July) 202[1234]$', line)
+            m = re.match(r' *(Mon|Tues|Wednes|Thurs|Fri)day,? \d+(nd|th)? (August|September|October|November|December|January|February|March|April|May|June|July) 202[1-5]$', line)
             if m:
                 date = line.strip() # datetime.strptime(line.strip(), '%A, %d %B %Y')
                 continue
