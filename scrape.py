e97fc06a7a1dc1cca589cf4a51ed79e07986176a
Matthew Somerville <matthew@mysociety.org>
2025-11-28 09:43:23 +0000

    Advert in footer.

--- scrape.py
+++ scrape.py
@@ -80,7 +80,7 @@ def fetch_hearing_page(item):
 def convert_four_up_pdf(text, date):
     # Remove header/footer from all pages
     text = re.sub('\014? *(The )?UK Covid-19 Inquiry  *\d+ .*? 202\d', '', text)
-    text = re.sub(' *\(\d+\) Pages \d+ - \d+', '', text)
+    text = re.sub(' *(Briault Reporting International - info@briaultreporting.com *)?\(\d+\) Pages \d+ - \d+', '', text)
     #text = re.sub('\xef\xbf\xbd', '', text)
 
     # Loop through, slurping up the pages by page number
